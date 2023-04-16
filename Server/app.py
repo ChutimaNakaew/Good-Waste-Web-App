6e0803ead64e935f9b62cfaf8ccd03603769929c
fah <63070039@kmitl.ac.th>
2023-04-17 01:44:47 +0700

    add

---
+++ Server/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder="")
+
+
+@app.route('/')
+def home():
+   return render_template("../Web/home.html")
+    
+
+@app.route('/rewards')
+def rewards():
+    return render_template("../Web/reward.html")
+    
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port="8081")
+
+
+    
