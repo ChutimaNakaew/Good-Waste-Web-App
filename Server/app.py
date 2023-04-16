f459df227f3594a412227f5616b91285561d634a
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-04-17 03:11:23 +0700

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__,template_folder="../Web")
+app = Flask(__name__,template_folder="")
 
 
 @app.route('/home')
 def home():
-   return render_template("index.html")
+   return render_template("../Web/index.html")
     
 
 @app.route('/rewards')
 def rewards():
-    return render_template("reward.html")
+    return render_template("../Web/reward.html")
     
 
 if __name__ == '__main__':
