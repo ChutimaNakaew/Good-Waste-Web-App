3cb0e83f7030119879b8c4441640329ffa9f5029
momo <73680098+YamonpornChertbamrung@users.noreply.github.com>
2023-04-17 04:18:27 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -1,11 +1,7 @@
-from typing import Union
-from fastapi import APIRouter
+
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from os.path import dirname, join
-import os
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="../Web")
@@ -15,6 +11,6 @@ async def home(request: Request):
 	return templates.TemplateResponse("index.html",{"request":request})
 
 
-@app.get("/test")
+@app.get("/reward")
 async def home(request: Request):
 	return templates.TemplateResponse("reward.html",{"request":request})
