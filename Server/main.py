cc8c9658523cf0b4a1a98a9d65f9603a1b7fe01c
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-04-17 22:35:33 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -6,6 +6,10 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="../Web")
 
+@app.get("/test")
+async def home(request: Request):
+	return "Hello"
+
 @app.get("/")
 async def home(request: Request):
 	return templates.TemplateResponse("index.html",{"request":request})
