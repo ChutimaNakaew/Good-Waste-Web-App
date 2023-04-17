4a81ce928ec334d235aba9a43915c6f755ff8ed1
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-04-17 23:20:04 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -3,17 +3,23 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader("Web"),
+    autoescape=select_autoescape()
+)
+templates = env.get_template("reward.html")
 app = FastAPI()
-templates = Jinja2Templates(directory="../Web")
+# templates = Jinja2Templates(directory="../Web")
 
 @app.get("/test")
 async def test(request: Request):
 	return "Test"
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def home(request: Request):
 	return templates.TemplateResponse("index.html",{"request":request})
 
-@app.get("/reward", response_class=HTMLResponse)
+@app.get("/reward")
 async def home(request: Request):
 	return templates.TemplateResponse("reward.html",{"request":request})
