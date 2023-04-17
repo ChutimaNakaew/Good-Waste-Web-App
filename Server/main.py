25101a6163e15f08f2158b5262c371475860e40a
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-04-17 23:08:14 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -2,27 +2,17 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
-# app = FastAPI()
-# templates = Jinja2Templates(directory="../Web")
+app = FastAPI()
+templates = Jinja2Templates(directory="../Web")
 
-from flask import Flask, render_template
-app = Flask(__name__)
+@app.get("/test")
+async def test(request: Request):
+	return employee.html
 
-@app.route('/')
-def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+@app.get("/")
+async def home(request: Request):
+	return templates.TemplateResponse("index.html",{"request":request})
 
-# @app.get("/test")
-# async def test(request: Request):
-# 	return employee.html
-
-# @app.get("/")
-# async def home(request: Request):
-# 	return templates.TemplateResponse("index.html",{"request":request})
-
-
-# @app.get("/reward")
-# async def home(request: Request):
-# 	return templates.TemplateResponse("reward.html",{"request":request})
+@app.get("/reward")
+async def home(request: Request):
+	return templates.TemplateResponse("reward.html",{"request":request})
