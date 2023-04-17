cb7d5b7772cadd89cadfb3ea7113a342b95689b2
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-04-17 22:38:53 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 templates = Jinja2Templates(directory="../Web")
 
 @app.get("/test")
-async def home(request: Request):
-	return "Hello"
+async def test(request: Request):
+	return "employee.html"
 
 @app.get("/")
 async def home(request: Request):
